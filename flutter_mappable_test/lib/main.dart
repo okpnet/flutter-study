33da10a6763bcdbe4f1ba20c62e858f25ae9cc3f
umai-beer <umai-beer@hotmail.co.jp>
2025-11-05 22:08:46 +0900

    ok

--- flutter_mappable_test/lib/main.dart
+++ flutter_mappable_test/lib/main.dart
@@ -27,7 +27,8 @@ void main() {
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+  
+  
   @override
   Widget build(BuildContext context,WidgetRef ref) {
     final themeData = ref.watch(themeDataProvider);
