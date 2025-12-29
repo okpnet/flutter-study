5d34c85e1ea2ffeef4f181d34da43db707ec7faf
hayato <umai-beer@hotmail.co.jp>
2025-12-29 17:03:08 +0900

    add a kecloak model the project.

--- flutter_win_webview/lib/extends/keycloak_access_model.dart
+++ flutter_win_webview/lib/extends/keycloak_access_model.dart
@@ -7,7 +7,7 @@
         'code_challenge': challenge,
         'code_challenge_method': 'S256',
 class KeycloakAccessModel {
-  const String _codeCharengeMethod = 'S256';
+  static const String _codeCharengeMethod = 'S256';
   final String authorizationEndpoint;
   final String tokenEndpoint;
   final String clientId;
@@ -22,5 +22,46 @@ class KeycloakAccessModel {
     required this.clientId,
     required this.redirectUri,
     required this.scopes,
-  });
+  }){
+
+  }
+
+  Map<String,dynamic> createUrlParameter(){
+    final result={
+      'response_type':'code',
+      'client_id':clientId,
+      'redirect_uri':redirectUri.toString(),
+      'scope':scopes.join(' '),
+      'code_challenge':codeChallenge,
+      'code_challenge_method':_codeCharengeMethod,
+    };
+    return result;
+  }
+
+  Uri get authorizationUrl {
+    final uri = Uri.parse(authorizationEndpoint);
+    final newUri = uri.replace(queryParameters: createUrlParameter());
+    return newUri;
+  }
+
+  
+  KeycloakAccessModel._(this.codeVerifier, this.codeChallenge);
+
+  static KeycloakAccessModel generate() {
+    String generateCodeVerifier() {
+      final random = Random.secure();
+      final values = List<int>.generate(32, (i) => random.nextInt(256));
+      return base64UrlEncode(values).replaceAll('=', '');
+    }
+
+    String generateCodeChallenge(String verifier) {
+      final bytes = utf8.encode(verifier);
+      final digest = sha256.convert(bytes);
+      return base64UrlEncode(digest.bytes).replaceAll('=', '');
+    }
+
+    final v = generateCodeVerifier();
+    final c = generateCodeChallenge(v);
+    return PKCEModel._(v, c);
+  }
 }
