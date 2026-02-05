60a8f624496fb81b05ceff36507a16125afff54d
hayato <umai-beer@hotmail.co.jp>
2026-02-05 17:01:09 +0900

    終了

--- flutter_win_webview/lib/screenlibs/overlay_loading.dart
+++ flutter_win_webview/lib/screenlibs/overlay_loading.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class OverlayLoading extends StatelessWidget {
   //表示状態
   final bool visible;
-  const OverlayLoading({required this.visible});
+  const OverlayLoading({super.key, required this.visible});
   @override
   Widget build(BuildContext context) {
     return visible
         ? Container(
-            decoration: new BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.6)),
+            decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.6)),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
--- flutter_win_webview/lib/start_app.dart
+++ flutter_win_webview/lib/start_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_win_webview/providers/auth_providers/auth_state.dart';
 import 'package:flutter_win_webview/providers/inialize.dart';
