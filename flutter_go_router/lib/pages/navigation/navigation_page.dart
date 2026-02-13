bc5cbd25e2c16c104a00e965e234f19cab78c087
umai-beer <umai-beer@hotmail.co.jp>
2026-02-13 22:52:40 +0900

    ok

---
+++ flutter_go_router/lib/pages/navigation/navigation_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_go_router/pages/navigation/app_navigation_bar.dart';
+import 'package:go_router/go_router.dart';
+
+class NavigationPage extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+  const NavigationPage({required this.navigationShell, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        body: navigationShell,
+        bottomNavigationBar: AppNavigationBar(navigationShell: navigationShell),
+      ),
+    );
+  }
+}
